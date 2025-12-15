ecdeccf7452af13952f45886ab0d35deeba27073
Naveen S <naveens@arizona.edu>
2025-12-15 15:58:24 -0500

    update loggin in ingestor.py

--- src/f1_data/ingestion/ingestor.py
+++ src/f1_data/ingestion/ingestor.py
@@ -74,12 +74,4 @@ class F1DataIngestor:
 
             response = self.fetch_page(season,limit,offset)
             self._save_page_minio(response,season,page)
-
-if __name__ == "__main__":
-
-    # Quick test to see if it works
-    ingestor = F1DataIngestor()
-    logger.info(f"\nSession created: {ingestor.session}")
-    season = int(input("\nenter the Season: "))
-    ingestor.ingest_season(season)
     
